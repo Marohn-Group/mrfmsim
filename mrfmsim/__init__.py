3e055e5710a7e6e120910501efdaabaccc4fbce2
Peter Sun <hs859@cornell.edu>
2024-06-20 20:31:05 -0400

    Add mrfmsim specific Graph object

--- mrfmsim/__init__.py
+++ mrfmsim/__init__.py
@@ -5,8 +5,10 @@ from mrfmsim.plugin import (
 )
 from mrfmsim.model import Experiment
 from mrfmsim.node import Node
+from mrfmsim.graph import Graph
 from mrfmsim.collection import ExperimentCollection
-from mmodel import Graph
+
+# from mmodel import Graph
 import sys
 
 # load the plugins at startup
@@ -14,11 +16,13 @@ HOST_MODULE = sys.modules[__name__]
 PLUGINS = PluginRegistry()
 LOADED_PLUGINS = PluginRegistry()
 
+
 def get_plugins():
     """Get the plugins."""
     global PLUGINS
     if not PLUGINS:
         register_plugins(group="mrfmsim_plugin", registry=PLUGINS)
+
 
 def load_plugins(**kwargs):
     """Load plugins that are based on the plugin tuple."""
---
+++ mrfmsim/graph.py
@@ -0,0 +1,7 @@
+import mmodel
+
+
+class Graph(mmodel.Graph):
+    """Graph object."""
+
+    graph_attr_dict_factory = {"type": "mrfmsim"}.copy
