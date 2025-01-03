db04f216fb791e5094e3bf86ba6ebd65a66cd97f
Peter Sun <peterhs73@outlook.com>
2025-01-02 23:49:47 -0500

    Change experiment parameters

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,22 +7,13 @@ and test functions.
 import pytest
 import math
 from mmodel.modifier import loop_input
-from mrfmsim import Experiment, Graph, Node
+from mrfmsim import Experiment, Graph, Node, ExperimentGroup
 import numpy as np
 import operator
 
-
 @pytest.fixture
-def modelgraph():
-    """Model graph for creating experiment and model.
-
-    The results are:
-    k = (a + h - d)(a + h)^f
-    m = log(a + h, b)
-
-    h defaults to 2
-    """
-
+def node_edges():
+    """Nodes and edges for creating graph."""
     grouped_edges = [
         ("add", ["subtract", "power", "log"]),
         (["subtract", "power"], "multiply"),
@@ -44,6 +35,21 @@ def modelgraph():
         Node("log", math.log, inputs=["c", "b"], output="m"),
     ]
 
+    return grouped_edges, node_objects
+
+@pytest.fixture
+def modelgraph(node_edges):
+    """Model graph for creating experiment and model.
+
+    The results are:
+    k = (a + h - d)(a + h)^f
+    m = log(a + h, b)
+
+    h defaults to 2
+    """
+
+    grouped_edges, node_objects = node_edges
+
     G = Graph(name="test_graph")
     G.add_grouped_edges_from(grouped_edges)
     G.set_node_objects_from(node_objects)
@@ -54,7 +60,7 @@ def modelgraph():
 @pytest.fixture
 def experiment(modelgraph):
     """Test experiment instance with default settings."""
-    return Experiment("test_experiment_plain", modelgraph, defaults={"h": 2})
+    return Experiment("test_experiment_plain", modelgraph, param_defaults={"h": 2})
 
 
 @pytest.fixture
@@ -67,5 +73,5 @@ def experiment_mod(modelgraph):
         components={"replace_obj": [("a", "a1"), ("b", "b1")]},
         modifiers=[loop_input(parameter="d")],
         doc="Test experiment with components.",
-        defaults={"h": 2},
+        param_defaults={"h": 2},
     )
