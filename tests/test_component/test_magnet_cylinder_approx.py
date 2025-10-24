9032701bf01077196a58943b7c64c2c850558e57
SKeeTElor <zw748@cornell.edu>
2025-10-23 21:50:59 -0400

    Modulate the test of CylinderMagnetApproxByRect
    The old test rely on CylinderMagnet, which is deleted now

--- tests/test_component/test_magnet_cylinder_approx.py
+++ tests/test_component/test_magnet_cylinder_approx.py
@@ -6,7 +6,7 @@ from mrfmsim.component import CylinderMagnet, CylinderMagnetApproxByRect
 """Test CylinderMagnetApproxByRect module in mrfmsim.component.
 
 
-CylinderMagnet & CylinderMagnetApproxByRect
+CylinderMagnetApproxByRect
 ------------
 
 All sphere magnets are tested using the parameters:
@@ -44,99 +44,85 @@ Bzxx_method
 
 """
 
+
+
 class TestCylinderMagnet:
     def setup_method(self):
-        self.magnet = CylinderMagnet(
-            magnet_radius=0.5, magnet_length=10, magnet_origin=[0, 0, 0], mu0_Ms=1
-        )
-        self.magnet_approx = CylinderMagnetApproxByRect(
+        self.magnet = CylinderMagnetApproxByRect(
             magnet_radius=0.5, magnet_length=10, magnet_origin=[0, 0, 0], mu0_Ms=1
         )
 
-    # test the shape of the magnetic field
+    # test the shape of the magnetic field to be the same as the input grid
     def test_Bz_shape(self):
         '''Test the shape of the magnetic field Bz to be the same as the input grid'''
         grid = np.ogrid[-1:1:2j, -1:1:5j, -1:1:10j]
-        Bz = self.magnet_approx.Bz_method(grid[0], grid[1], grid[2])
+        Bz = self.magnet.Bz_method(grid[0], grid[1], grid[2])
         assert Bz.shape == (2, 5, 10)
 
     def test_Bzx_shape(self):
         '''Test the shape of the magnetic field Bzx to be the same as the input grid'''
         grid = np.ogrid[-1:1:2j, -1:1:5j, -1:1:10j]
-        Bzx = self.magnet_approx.Bzx_method(grid[0], grid[1], grid[2])
+        Bzx = self.magnet.Bzx_method(grid[0], grid[1], grid[2])
         assert Bzx.shape == (2, 5, 10)
 
     def test_Bzxx_shape(self):
         '''Test the shape of the magnetic field Bzxx to be the same as the input grid'''
         grid = np.ogrid[-1:1:2j, -1:1:5j, -1:1:10j]
-        Bzxx = self.magnet_approx.Bzxx_method(grid[0], grid[1], grid[2])
+        Bzxx = self.magnet.Bzxx_method(grid[0], grid[1], grid[2])
         assert Bzxx.shape == (2, 5, 10)
 
-    # test the symmetry of the magnetic field
+    # test the symmetry of the magnetic field along x-axis
     def test_Bz_symmetry(self):
-        '''Test the x-axis symmetry of the magnetic field Bz'''
-        Bz1 = self.magnet_approx.Bz_method(1, 2, 6)
-        Bz2 = self.magnet_approx.Bz_method(-1, 2, 6)
+        '''Test the symmetry of the magnetic field Bz along x-axis'''
+        Bz1 = self.magnet.Bz_method(1, 2, 6)
+        Bz2 = self.magnet.Bz_method(-1, 2, 6)
         assert np.allclose(Bz1, Bz2)
 
     def test_Bzx_symmetry(self):
-        '''Test the x-axis antisymmetry of the magnetic field Bzx'''
-        Bzx1 = self.magnet_approx.Bzx_method(1, 2, 6)
-        Bzx2 = self.magnet_approx.Bzx_method(-1, 2, 6)
+        '''Test the symmetry of the magnetic field Bzx along x-axis'''
+        Bzx1 = self.magnet.Bzx_method(1, 2, 6)
+        Bzx2 = self.magnet.Bzx_method(-1, 2, 6)
         assert np.allclose(Bzx1, -Bzx2)  # Bzx is antisymmetric
 
     def test_Bzxx_symmetry(self):
-        '''Test the x-axis symmetry of the magnetic field Bzxx'''
-        Bzxx1 = self.magnet_approx.Bzxx_method(1, 2, 6)
-        Bzxx2 = self.magnet_approx.Bzxx_method(-1, 2, 6)
+        '''Test the symmetry of the magnetic field Bzxx along x-axis'''
+        Bzxx1 = self.magnet.Bzxx_method(1, 2, 6)
+        Bzxx2 = self.magnet.Bzxx_method(-1, 2, 6)
         assert np.allclose(Bzxx1, Bzxx2)  # Bzxx is symmetric
 
-    # test the value of the magnetic field
-    def test_Bz_NearField(self):
-        '''Test the near field of the magnetic field Bz to be close to the exact value'''
-        grid = np.ogrid[-3:3:10j, -3:3:10j, 6:7:2j]
-        Bz_approx = self.magnet_approx.Bz_method(grid[0], grid[1], grid[2])
-        Bz_exact = self.magnet.Bz_method(grid[0], grid[1], grid[2])
-        assert np.allclose(Bz_approx, Bz_exact, rtol=1e-3)
+    # test the value of the magnetic field at poles(1.0, 2.0, 6.0) & (10.0, 0.0, 0.0)
+    def test_Bz_1(self):
+        '''Test the magnetic field Bz at near field'''
+        Bz = self.magnet.Bz_method(1, 2, 6)
+        assert np.allclose(Bz, 0.00391404, atol=1e-5)
 
-    def test_Bzx_NearField(self):
-        '''Test the near field of the magnetic field Bzx to be close to the exact value'''
-        grid = np.ogrid[-3:3:10j, -3:3:10j, 6:7:2j]
-        Bzx_approx = self.magnet_approx.Bzx_method(grid[0], grid[1], grid[2])
-        Bzx_exact = self.magnet.Bzx_method(grid[0], grid[1], grid[2])
-        assert np.allclose(Bzx_approx, Bzx_exact, rtol=1e-3)
+    def test_Bzx_1(self):
+        '''Test the magnetic field Bzx at near field'''
+        Bzx = self.magnet.Bzx_method(1, 2, 6)
+        assert np.allclose(Bzx, -0.00221805, atol=1e-5)
 
-    def test_Bzxx_NearField(self):
-        '''Test the near field of the magnetic field Bzxx to be close to the exact value'''
-        grid = np.ogrid[-3:3:10j, -3:3:10j, 6:7:2j]
-        Bzxx_approx = self.magnet_approx.Bzxx_method(grid[0], grid[1], grid[2])
-        Bzxx_exact = self.magnet.Bzxx_method(grid[0], grid[1], grid[2])
-        assert np.allclose(Bzxx_approx, Bzxx_exact, rtol=1e-3)
+    def test_Bzxx_1(self):
+        '''Test the magnetic field Bzxx at near field'''
+        Bzxx = self.magnet.Bzxx_method(1, 2, 6)
+        assert np.allclose(Bzxx, 0.0031995877, atol=1e-5)
 
-    def test_Bz_FarField(self):
-        '''Test the far field of the magnetic field Bz to be close to the exact value'''
-        grid = np.ogrid[-13:-10:10j, -1:2:10j, 6:7:2j]
-        Bz_approx = self.magnet_approx.Bz_method(grid[0], grid[1], grid[2])
-        Bz_exact = self.magnet.Bz_method(grid[0], grid[1], grid[2])
-        assert np.allclose(Bz_approx, Bz_exact, atol=1e-4)
+    def test_Bz_2(self):
+        '''Test the magnetic field Bz at far field'''
+        Bz = self.magnet.Bz_method(10, 0, 0)
+        assert np.allclose(Bz, -0.00044788490737216163, atol=1e-5)
 
-    def test_Bzx_FarField(self):    
-        '''Test the far field of the magnetic field Bzx to be close to the exact value'''
-        grid = np.ogrid[-13:-10:10j, -1:2:10j, 6:7:2j]
-        Bzx_approx = self.magnet_approx.Bzx_method(grid[0], grid[1], grid[2])
-        Bzx_exact = self.magnet.Bzx_method(grid[0], grid[1], grid[2])
-        assert np.allclose(Bzx_approx, Bzx_exact, atol=1e-6)
+    def test_Bzx_2(self):
+        '''Test the magnetic field Bzx at far field'''
+        Bzx = self.magnet.Bzx_method(10, 0, 0)
+        assert np.allclose(Bzx, 0.00010754599, atol=1e-5)
 
-    def test_Bzxx_FarField(self):
-        '''Test the far field of the magnetic field Bzxx to be close to the exact value'''
-        grid = np.ogrid[-13:-10:10j, -1:2:10j, 6:7:2j]
-        Bzxx_approx = self.magnet_approx.Bzxx_method(grid[0], grid[1], grid[2])
-        Bzxx_exact = self.magnet.Bzxx_method(grid[0], grid[1], grid[2])
-        assert np.allclose(Bzxx_approx, Bzxx_exact, atol=1e-6)
-
+    def test_Bzxx_2(self):
+        '''Test the magnetic field Bzxx at far field'''
+        Bzxx = self.magnet.Bzxx_method(10, 0, 0)
+        assert np.allclose(Bzxx, -3.22679905171e-05, atol=1e-5)
 
     # test the when x is 0, Bzx is 0
     def test_Bzx_when_x_is_0(self):
-        '''Test the magnetic field Bzx when x is 0 to be 0'''
-        Bzx = self.magnet_approx.Bzx_method(0, 10, 0)
+        '''Test the magnetic field Bzx when x is 0'''
+        Bzx = self.magnet.Bzx_method(0, 10, 0)
         assert np.allclose(Bzx, 0, atol=1e-10)
